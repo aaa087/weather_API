402fef23894381b60f1c066149f4a8d9bd0e2946
toshiba <yanshi.panda18@icloud.com>
2022-06-25 21:52:23 +0900

    weather_API

---
+++ weather/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import TopView,WeatherAPIView
+
+urlpatterns = [
+    path('top/',TopView.as_view()),
+    path('api/<int:pk>/',WeatherAPIView.as_view()),
+]
